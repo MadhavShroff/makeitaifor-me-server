2c86414a4eb37a8377cceab858378086766bf5c3
MadhavShroff <madhavshroff99@gmail.com>
2023-07-04 22:03:27 -0400

    CI/CD

--- redeploy.sh
+++ redeploy.sh
@@ -1,6 +1,7 @@
 homeDir="/home/ubuntu/makeitaifor-me-server";
 
-/home/ubuntu/.nvm/versions/node/v18.16.1/bin/npm install; /home/ubuntu/.nvm/versions/node/v18.16.1/bin/npm run build;
+/home/ubuntu/.nvm/versions/node/v18.16.1/bin/npm install; 
+/home/ubuntu/.nvm/versions/node/v18.16.1/bin/npm run build;
 
-/home/ubuntu/.nvm/versions/node/v19.0.1/bin/pm2 stop "ai-server"; # stop server if running
-/home/ubuntu/.nvm/versions/node/v19.0.1/bin/pm2 start $homeDir/dist/main.js --name "ai-server"; # start server
+/home/ubuntu/.nvm/versions/node/v18.16.1/bin/pm2 stop "ai-server"; # stop server if running
+/home/ubuntu/.nvm/versions/node/v18.16.1/bin/pm2 start $homeDir/dist/main.js --name "ai-server"; # start server
