161222a2fb4abfb5bcbfd97265629573cebf19f8
MadhavShroff <madhavshroff99@gmail.com>
2023-07-05 18:19:34 -0400

    Added POST /exchange to swap auth code for tokens

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,31 @@
 import { Injectable } from '@nestjs/common';
-import axios from 'axios';
+import * as dotenv from 'dotenv';
+import axios, { AxiosResponse } from 'axios';
+import * as querystring from 'querystring';
+
+dotenv.config();
 
 @Injectable()
 export class AuthService {
-  async exchangeCodeForTokens(code: string) {
+  async exchangeCodeForTokens(code: string): Promise<AxiosResponse> {
+    const data = querystring.stringify({
+      grant_type: 'authorization_code',
+      client_id: process.env.OAUTH_COGNITO_ID,
+      code: code,
+      redirect_uri: process.env.OAUTH_COGNITO_REDIRECT_URL,
+    });
+    const auth = {
+      username: process.env.OAUTH_COGNITO_ID,
+      password: process.env.OAUTH_COGNITO_SECRET,
+    };
+    const headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    };
     const response = await axios.post(
-      `https://your-domain.auth.us-east-1.amazoncognito.com/oauth2/token?grant_type=authorization_code&client_id=your_app_client_id&code=${code}&redirect_uri=https://your_redirect_uri`,
-      {},
-      { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } },
+      `https://${process.env.OAUTH_COGNITO_DOMAIN}.auth.${process.env.OAUTH_COGNITO_REGION}.amazoncognito.com/oauth2/token`,
+      data,
+      { auth, headers },
     );
-    const tokens = response.data;
-    return tokens;
+    return response;
   }
 }
