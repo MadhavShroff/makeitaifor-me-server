eeb4a1551bc64331e668b927685d3b5cad30736e
MadhavShroff <madhavshroff99@gmail.com>
2023-09-12 12:28:46 -0400

    added body parser

--- dist/main.js
+++ dist/main.js
@@ -5,13 +5,14 @@ const app_module_1 = require("./app.module");
 const cookieParser = require("cookie-parser");
 const common_1 = require("@nestjs/common");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const app = (await core_1.NestFactory.create(app_module_1.AppModule));
     app.enableCors({
         origin: ['https://www.makeitaifor.me', 'http://localhost:3001'],
         methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
         credentials: true,
     });
     app.use(cookieParser());
+    app.useBodyParser('json', { limit: '10mb' });
     app.useGlobalPipes(new common_1.ValidationPipe());
     await app.listen(3000);
 }
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import * as csurf from 'csurf';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 // import { SpelunkerModule } from 'nestjs-spelunker';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app: NestExpressApplication = (await NestFactory.create(
+    AppModule,
+  )) as NestExpressApplication;
   // 1. Generate the tree as text
   // const tree = SpelunkerModule.explore(app);
   // const root = SpelunkerModule.graph(tree);
@@ -78,6 +81,7 @@ async function bootstrap() {
     credentials: true,
   });
   app.use(cookieParser());
+  app.useBodyParser('json', { limit: '10mb' });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
